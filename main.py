ab6b51e25c09ffa5c7011bd516b80773c9811267
christianromo1 <chromofps@gmail.com>
2025-03-22 19:51:04 -0500

    Integrated LLM model Llama 3.2 to AI Wen Scraper

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from scrape import (
     clean_body_content, 
     extract_body_content
 )
+from parse import parse_with_ollama
 
 st.title("AI Web Scraper")
 url = st.text_input("Enter a Website URL: ")
@@ -20,3 +21,15 @@ if st.button("Scrape Site"):
 
     with st.expander("View DOM Content"):
         st.text_area("DOM Content", cleaned_content, height=300)
+
+
+if "dom_content" in st.session_state:
+    parse_description = st.text_area("Describe what you want to parse?")
+
+    if st.button("Parse Content"):
+        if parse_description:
+            st.write("Parsing the content")
+
+            dom_chunks = split_dom_content(st.session_state.dom_content)
+            result = parse_with_ollama(dom_chunks, parse_description)
+            st.write(result)
